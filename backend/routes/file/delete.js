e90c5ce4e0669f9cee4aed36e0c6515e503e3fc6
BuzaAlx <buzakivi@gmail.com>
2022-08-07 19:16:23 +0300

    delete image route

---
+++ backend/routes/file/delete.js
@@ -0,0 +1,41 @@
+const yup = require("yup");
+const { File } = require("../../database");
+const { deleteFile } = require("../../s3");
+
+const fileDelete = {
+  path: "/:id",
+  method: "DELETE",
+  validate: {
+    params: {
+      schema: yup.object({
+        id: yup.objectId().required(),
+      }),
+    },
+  },
+  handler: async (context) => {
+    const { params } = context.request;
+
+    const f = await File.findOne().where("_id", params.id).exec();
+
+    await deleteFile(f);
+
+    const file = await File.findByIdAndDelete(params.id);
+
+    if (!file) {
+      context.status = 404;
+
+      context.body = {
+        response: null,
+      };
+
+      return;
+    }
+
+    context.body = {
+      response: "succesfully deletet",
+    };
+    context.status = 204;
+  },
+};
+
+exports.fileDelete = fileDelete;
