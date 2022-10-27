f439dca129f591312bde890b23284c70a7b97aa3
BuzaAlx <buzakivi@gmail.com>
2022-10-27 13:04:17 +0300

    deploy on vercel

--- backend/s3.js
+++ backend/s3.js
@@ -11,8 +11,8 @@ const fs = require("fs");
 
 const bucketName = process.env.AWS_BUCKET_NAME;
 const region = process.env.AWS_BUCKET_REGION;
-const accessKeyId = process.env.AWS_ACCESS_KEY;
-const secretAccessKey = process.env.AWS_SECRET_KEY;
+const accessKeyId = process.env.AWS_ACCESSBUCKET_KEY;
+const secretAccessKey = process.env.AWS_SECRETBUCKET_KEY;
 
 const s3Client = new S3Client({
   region,
