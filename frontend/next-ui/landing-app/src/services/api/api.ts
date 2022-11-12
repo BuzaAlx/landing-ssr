e4b2940c1b278becf158c0a0a68a38fafb7f4b9f
BuzaAlx <buzakivi@gmail.com>
2022-11-12 22:42:22 +0200

    home page API

---
+++ frontend/next-ui/landing-app/src/services/api/api.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const BASE_URL: string | undefined = "http://localhost:7000";
+
+axios.defaults.baseURL = BASE_URL;
+axios.defaults.params = {};
+
+const API_ROUTES = {
+  workers: "/home/get-workers",
+  projects: `/home/get-projects`,
+  facts: "/home/get-facts",
+  technologies: "/home/get-technologies",
+  testimonials: "/home/get-testimonials",
+  slogan: `/home/get-slogan`,
+  articles: "/blog/",
+  article: "/blog/",
+  similarArticles: "/blog/similar-articles/",
+};
+
+export const getData = async (route: string, id?: string | undefined) => {
+  try {
+    const { data } = await axios.get(API_ROUTES[route] + (id ? id : ""));
+    const response = data.response;
+    return response;
+  } catch (error) {
+    console.log("error", { error });
+    return null;
+  }
+};
